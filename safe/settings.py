bc3bcb6016ac99b0624959e914a0ee174ef8f815
ahyaghifari <ahyaghifari288@gmail.com>
2022-12-10 09:16:05 +0800

    hosts

--- safe/settings.py
+++ safe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = ("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://safe.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['safe.up.railway.app', '127.0.0.1']
 
 # Application definition
 
