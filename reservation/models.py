70c11a8c74f1f4ec24c1e2f18206b3b022d801b5
ahyaghifari <ahyaghifari288@gmail.com>
2022-12-11 20:50:36 +0800

    update

--- reservation/models.py
+++ reservation/models.py
@@ -1,45 +1,3 @@
-# from django.db import models
-
-# class Audiens(models.Model):
-#     kuantitas = models.CharField(verbose_name="audiens", max_length=50)
-
-#     def __str__(self):
-#         return self.kuantitas
-
-
-# class Ruangan(models.Model):
-#     ruangan = models.CharField(max_length=50)
-
-#     def __str__(self):
-#         return self.ruangan
-
-# class WaktuPemesanan(models.Model):
-#     waktu = models.CharField(verbose_name="Waktu Pemesanan", max_length=50)
-
-#     def __str__(self):
-#         return self.waktu
-
-# class Reservation(models.Model):
-#     nama = models.CharField(
-#         verbose_name='Nama Pemesan', max_length=100)
-#     email = models.EmailField(
-#         verbose_name='Email Pemesan', max_length=254)
-#     telepon = models.CharField(
-#         verbose_name='Telepon/WA', max_length=50)
-#     acara = models.CharField(max_length=100)
-#     tanggal_pemesanan = models.DateField(
-#         verbose_name='Tanggal Acara')
-#     waktu_pemesanan = models.ForeignKey(WaktuPemesanan, on_delete=models.CASCADE)
-#     ruangan = models.ForeignKey(Ruangan, on_delete=models.CASCADE)
-#     audiens = models.ForeignKey(Audiens, on_delete=models.CASCADE)
-#     keterangan = models.TextField()
-#     konfirmasi = models.BooleanField(default=False)
-#     selesai = models.BooleanField(default=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.nama + self.acara + str(self.tanggal_pemesanan)
-
 from django.db import models
 
 class Audiens(models.Model):
@@ -55,6 +13,11 @@ class Ruangan(models.Model):
     def __str__(self):
         return self.ruangan
 
+class WaktuPemesanan(models.Model):
+    waktu = models.CharField(verbose_name="Waktu Pemesanan", max_length=50)
+
+    def __str__(self):
+        return self.waktu
 
 class Reservation(models.Model):
     nama = models.CharField(
@@ -63,10 +26,10 @@ class Reservation(models.Model):
         verbose_name='Email Pemesan', max_length=254)
     telepon = models.CharField(
         verbose_name='Telepon/WA', max_length=50)
+    acara = models.CharField(max_length=100)
     tanggal_pemesanan = models.DateField(
         verbose_name='Tanggal Acara')
-    waktu_pemesanan = models.TimeField(verbose_name='Waktu Acara')
-    acara = models.CharField(max_length=100)
+    waktu_pemesanan = models.ForeignKey(WaktuPemesanan, on_delete=models.CASCADE)
     ruangan = models.ForeignKey(Ruangan, on_delete=models.CASCADE)
     audiens = models.ForeignKey(Audiens, on_delete=models.CASCADE)
     keterangan = models.TextField()
@@ -76,3 +39,40 @@ class Reservation(models.Model):
 
     def __str__(self):
         return self.nama + self.acara + str(self.tanggal_pemesanan)
+
+# from django.db import models
+
+# class Audiens(models.Model):
+#     kuantitas = models.CharField(verbose_name="audiens", max_length=50)
+
+#     def __str__(self):
+#         return self.kuantitas
+
+
+# class Ruangan(models.Model):
+#     ruangan = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.ruangan
+
+
+# class Reservation(models.Model):
+#     nama = models.CharField(
+#         verbose_name='Nama Pemesan', max_length=100)
+#     email = models.EmailField(
+#         verbose_name='Email Pemesan', max_length=254)
+#     telepon = models.CharField(
+#         verbose_name='Telepon/WA', max_length=50)
+#     tanggal_pemesanan = models.DateField(
+#         verbose_name='Tanggal Acara')
+#     waktu_pemesanan = models.TimeField(verbose_name='Waktu Acara')
+#     acara = models.CharField(max_length=100)
+#     ruangan = models.ForeignKey(Ruangan, on_delete=models.CASCADE)
+#     audiens = models.ForeignKey(Audiens, on_delete=models.CASCADE)
+#     keterangan = models.TextField()
+#     konfirmasi = models.BooleanField(default=False)
+#     selesai = models.BooleanField(default=False)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.nama + self.acara + str(self.tanggal_pemesanan)
