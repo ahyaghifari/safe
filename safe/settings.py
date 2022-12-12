070476dbf841b9ea33b535c6567121c0c9a172bf
ahyaghifari <ahyaghifari288@gmail.com>
2022-12-12 09:28:39 +0800

    debug

--- safe/settings.py
+++ safe/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['safe.up.railway.app', '127.0.0.1']
 
